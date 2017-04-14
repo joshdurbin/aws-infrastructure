143674f8b194a71576aeb189425f005b11910b5b
Josh Durbin <joshua.durbin@simplytapp.com>
2017-04-13 20:05:20 -0700

    added base url configs for api gateway

--- dns.tf
+++ dns.tf
@@ -44,3 +44,17 @@ resource "aws_route53_record" "www_joshdurbin_net" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "apps_joshdurbin_net" {
+
+  name = "apps"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.id}"
+  type = "A"
+
+  alias {
+
+    name = "${aws_api_gateway_domain_name.apps_joshdurbin.cloudfront_domain_name}"
+    zone_id = "${aws_api_gateway_domain_name.apps_joshdurbin.cloudfront_zone_id}"
+    evaluate_target_health = true
+  }
+}
