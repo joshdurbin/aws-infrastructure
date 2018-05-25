6f03e6e4fcfeaabda915575c0d8561d1803f5c89
Josh Durbin <durbinjo593@gmail.com>
2018-05-25 07:18:44 -0700

    updates to dns

--- dns.tf
+++ dns.tf
@@ -60,21 +60,87 @@ resource "aws_route53_record" "keybase_verification_joshdurbin_net" {
   records = ["keybase-site-verification=3Xua_2l2_vi5onXlYIYi5WPnb_wL-vi5dqs010jZR7o"]
 }
 
-resource "aws_route53_record" "gsuite_proof" {
-
-  name = "15814602"
-  zone_id = "${aws_route53_zone.www_joshdurbin_net.id}"
-  type = "CNAME"
+resource "aws_route53_record" "g_suite_spf" {
+  name = ""
+  type = "TXT"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
   ttl = "${var.global_ttl}"
-
   records = [
-    "google.com"
+    "v=spf1 include:_spf.google.com ~all"
   ]
 }
 
-module "example_fastmail" {
-  source = "github.com/bluk/terraform-aws-fastmail-dns"
+resource "aws_route53_record" "g_suite_dkim" {
+  name = "google._domainkey"
+  type = "TXT"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuhEjsqG07ljLzjY0i/T7AXyX6n+s2x+Ef90gZEIcQqkZWMHGtcya\" \"gMBo4JB0kcDOoPzclpA6sHEZzOakDcRuI8MraRywrUF+bu8gSCz9lPAwN4mjF5Xm/ZIHMAxeHPyaTqaRJOTZpm2TX9eKvgf82BoKuJlFEHe2Jf8t5\" \"7qPAeVt/EbccPalNqrVe5WxRYcKgseT2BP3wdumJ6xXVuS1i/T3rEDDBj37P8GqKTfkp5NQITtXwW4IQwtUEyuXnh42LlstyYxsP6D95SBypTwPkC\" \"T70CUZ71H7xJ9Rd6zMLi6O/RKVL2pCvpR9PwLbVXr8oB1d5M40tS76yXDvQgblowIDAQAB"
+  ]
+}
 
-  domain_name = "${var.joshdurbin_net}"
-  route53_zone_id = "${aws_route53_zone.www_joshdurbin_net.id}"
+resource "aws_route53_record" "g_suite_mail_cname" {
+  name = "mail"
+  type = "CNAME"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "ghs.googlehosted.com"
+  ]
+}
+
+resource "aws_route53_record" "g_suite_drive_cname" {
+  name = "drive"
+  type = "CNAME"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "ghs.googlehosted.com"
+  ]
+}
+
+resource "aws_route53_record" "g_suite_calendar_cname" {
+  name = "calendar"
+  type = "CNAME"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "ghs.googlehosted.com"
+  ]
+}
+
+resource "aws_route53_record" "g_suite_sites_cname" {
+  name = "sites"
+  type = "CNAME"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "ghs.googlehosted.com"
+  ]
+}
+
+resource "aws_route53_record" "g_suite_groups_cname" {
+  name = "groups"
+  type = "CNAME"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "ghs.googlehosted.com"
+  ]
+}
+
+resource "aws_route53_record" "g_suite_mx" {
+
+  name = ""
+  type = "MX"
+  zone_id = "${aws_route53_zone.www_joshdurbin_net.zone_id}"
+  ttl = "${var.global_ttl}"
+  records = [
+    "1 aspmx.l.google.com",
+    "5 alt1.aspmx.l.google.com",
+    "5 alt2.aspmx.l.google.com",
+    "10 alt3.aspmx.l.google.com",
+    "10 alt4.aspmx.l.google.com",
+  ]
 }
