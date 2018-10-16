37e11c0861ebe2bb64d2b6969e61dc50c6cd92df
Joshua Durbin <durbinjo593@gmail.com>
2018-10-16 07:50:48 -0700

    adding iam rules for circleci keys

---
+++ iam.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_access_key" "circle_ci_deploy_key" {
+  user    = "${aws_iam_user.circle_ci.name}"
+}
+
+resource "aws_iam_user" "circle_ci" {
+  name = "circleci"
+  path = "/system/"
+}
+
+resource "aws_iam_user_policy" "circle_ci_s3_access" {
+  name = "s3_access"
+  user = "${aws_iam_user.circle_ci.name}"
+
+  policy = "${data.aws_iam_policy_document.circle_ci_s3_access_policy.json}"
+}
+
+data "aws_iam_policy_document" "circle_ci_s3_access_policy" {
+
+  statement {
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.www_joshdurbin_net.arn}",
+      "${aws_s3_bucket.www_joshdurbin_net.arn}/*"
+    ]
+  }
+}
+
+output "circle_ci_keys" {
+  value = "${aws_iam_access_key.circle_ci_deploy_key.id} -- ${aws_iam_access_key.circle_ci_deploy_key.secret}"
+}
--- variables.tf
+++ variables.tf
@@ -22,18 +22,6 @@ variable "default_google_zone" {
   default = "us-west1-a"
 }
 
-variable "gcp_node_type" {
-  default = "n1-standard-1"
-}
-
-variable "gcp_cluster_count" {
-  default = "1"
-}
-
-variable "cluster_name" {
-  default = "test"
-}
-
 variable "digital_ocean_mail_server_machine_image" {
   default = "debian-9-x64"
 }
@@ -53,8 +41,4 @@ variable "global_ttl" {
 variable "digital_ocean_token" {}
 
 variable "www_joshdurbin_net_certificate_id" {}
-variable "canary_username" {}
-variable "canary_encrypted_password" {}
 
-variable "k8s_cluster_linux_admin_username" {}
-variable "k8s_cluster_linux_admin_password" {}
